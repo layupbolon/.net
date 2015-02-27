d72959655181e0d185296750cc8628defdfcc214
layupbolon <20732904@qq.com>
2015-02-27 10:06:31 +0800

    修改了备忘录模式

--- 备忘录模式/Program.cs
+++ 备忘录模式/Program.cs
@@ -4,68 +4,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ 定义：在不破坏封装的前提下，捕获一个对象的内部状态，并在该对象之外保存这个状态，这样以后就可以把该对象恢复到原先的状态。
+ 优点：
+    •如果某个操作错误地破坏了数据的完整性，此时可以使用备忘录模式将数据恢复成原来正确的数据。
+    •备份的状态数据保存在发起人角色之外，这样发起人就不需要对各个备份的状态进行管理。而是由备忘录角色进行管理，而备忘录角色又是由管理者角色管理，符合单一职责原则。
+ 缺点：
+    •在实际的系统中，可能需要维护多个备份，需要额外的资源，这样对资源的消耗比较严重。
+ 适用场景：
+    •如果系统需要提供回滚操作时，使用备忘录模式非常合适。例如文本编辑器的Ctrl+Z撤销操作的实现，数据库中事务操作。
+ */
+
 namespace 备忘录模式
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //List<ContactPerson> contactPersons = new List<ContactPerson>()
-            //{
-            //    new ContactPerson(){Name="张三",MobileNumber="123456789"},
-            //    new ContactPerson(){Name="李四",MobileNumber="987498416"},
-            //    new ContactPerson(){Name="王五",MobileNumber="284589891"}
-            //};
-
-            //MobileOwner owner = new MobileOwner(contactPersons);
-            //Administrator admin = new Administrator();
-            //admin.MemetoDictionary.TryAdd(DateTime.Now.ToString(), owner.CreateMemento());
-            //owner.Show();
-
-            //Console.WriteLine("--------------------------------------");
-            //Console.WriteLine("删掉王五联系人");
-            //Console.WriteLine("--------------------------------------");
-            //owner.ContactPersons.RemoveAt(2);
-            //admin.MemetoDictionary.TryAdd(DateTime.Now.ToString(), owner.CreateMemento());
-            //owner.Show();
-
-            //Console.WriteLine("--------------------------------------");
-            //Console.WriteLine("还原之前备份的联系人列表");
-
-            //Memento memento = new Memento(null);
-            //if (admin.MemetoDictionary.TryGetValue(admin.MemetoDictionary.Keys.First(), out memento))
-            //{
-            //    owner.RestoreMemeto(memento);
-
-            //    Console.WriteLine("--------------------------------------");
-            //    owner.Show();
-            //}
-            //else
-            //    Console.WriteLine("获取备份失败");
-
-
-            System.Collections.Concurrent.ConcurrentDictionary<int, int> dictionary = new System.Collections.Concurrent.ConcurrentDictionary<int, int>();
-
-            var actions = new List<Action>();
-            for (int i = 0; i < 10000; i++)
+            List<ContactPerson> contactPersons = new List<ContactPerson>()
             {
-                actions.Add(() => { dictionary.TryAdd(i, i); });
-            }
+                new ContactPerson(){Name="张三",MobileNumber="123456789"},
+                new ContactPerson(){Name="李四",MobileNumber="987498416"},
+                new ContactPerson(){Name="王五",MobileNumber="284589891"}
+            };
 
-            var result = (from a in actions.AsParallel().WithDegreeOfParallelism(5)
-                select a).ToArray();
+            MobileOwner owner = new MobileOwner(contactPersons);
+            Administrator admin = new Administrator();
+            admin.MemetoDictionary.TryAdd(DateTime.Now.ToString(), owner.CreateMemento());
+            owner.Show();
 
-            Parallel.Invoke(result);
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("删掉王五联系人");
+            Console.WriteLine("--------------------------------------");
+            owner.ContactPersons.RemoveAt(2);
+            admin.MemetoDictionary.TryAdd(DateTime.Now.ToString(), owner.CreateMemento());
+            owner.Show();
 
-            if (dictionary.Count>0)
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("还原之前备份的联系人列表");
+
+            Memento memento = new Memento(null);
+            if (admin.MemetoDictionary.TryGetValue(admin.MemetoDictionary.Keys.First(), out memento))
             {
-                foreach (KeyValuePair<int, int> keyValuePair in dictionary)
-                {
-                    Console.WriteLine("key:{0} value:{1}", keyValuePair.Key, keyValuePair.Value);
-                }
+                owner.RestoreMemeto(memento);
+
+                Console.WriteLine("--------------------------------------");
+                owner.Show();
             }
             else
-                Console.WriteLine("字典为空");
+                Console.WriteLine("获取备份失败");
+
+
+            //System.Collections.Concurrent.ConcurrentDictionary<int, int> dictionary = new System.Collections.Concurrent.ConcurrentDictionary<int, int>();
+
+            //var actions = new List<Action>();
+            //for (int i = 0; i < 10000; i++)
+            //{
+            //    actions.Add(() => { dictionary.TryAdd(i, i); });
+            //}
+
+            //var result = (from a in actions.AsParallel().WithDegreeOfParallelism(5)
+            //    select a).ToArray();
+
+            //Parallel.Invoke(result);
+
+            //if (dictionary.Count>0)
+            //{
+            //    foreach (KeyValuePair<int, int> keyValuePair in dictionary)
+            //    {
+            //        Console.WriteLine("key:{0} value:{1}", keyValuePair.Key, keyValuePair.Value);
+            //    }
+            //}
+            //else
+            //    Console.WriteLine("字典为空");
 
             Console.ReadKey();
         }
